5c4faf15f42d073b7c857abdaaccd8f455f93b69
basant44 <basant.ashraf@yahoo.com>
2015-07-28 01:33:54 -0700

    message

---
+++ GitUseExample2/MapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.h
+//  GitUseExample2
+//
+//  Created by alaa-ibtikar on 7/28/15.
+//  Copyright (c) 2015 alaa-ibtikar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternerary : NSObject
+
+@end
---
+++ GitUseExample2/MapForIternerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.m
+//  GitUseExample2
+//
+//  Created by alaa-ibtikar on 7/28/15.
+//  Copyright (c) 2015 alaa-ibtikar. All rights reserved.
+//
+
+#import "MapForIternerary.h"
+
+@implementation MapForIternerary
+
+@end
