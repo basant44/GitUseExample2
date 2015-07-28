c1fa4e3bafec64180ae00c059949c73caaa65d88
basant44 <basant.ashraf@yahoo.com>
2015-07-28 02:23:56 -0700

    ccccc

---
+++ GitUseExample2/class2.h
@@ -0,0 +1,13 @@
+//
+//  class2.h
+//  GitUseExample2
+//
+//  Created by alaa-ibtikar on 7/28/15.
+//  Copyright (c) 2015 alaa-ibtikar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface class2 : NSObject
+
+@end
---
+++ GitUseExample2/class2.m
@@ -0,0 +1,13 @@
+//
+//  class2.m
+//  GitUseExample2
+//
+//  Created by alaa-ibtikar on 7/28/15.
+//  Copyright (c) 2015 alaa-ibtikar. All rights reserved.
+//
+
+#import "class2.h"
+
+@implementation class2
+
+@end
